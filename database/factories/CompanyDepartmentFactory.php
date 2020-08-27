ad96e85637bb191ce4e89ca850ef541489d728d4
bosunski <bosunski@gmail.com>
2020-08-27 17:06:56 +0100

    Add Database Seeder

--- database/factories/CompanyDepartmentFactory.php
+++ database/factories/CompanyDepartmentFactory.php
@@ -5,15 +5,7 @@
 use App\CompanyDepartment;
 use Faker\Generator as Faker;
 
-$sampleDepartments = [
-    'Cleaning',
-    'Engineering',
-    'Data Science',
-    'Human Resources',
-    'User Experience',
-    'Automation',
-    'Sales'
-];
+$sampleDepartments = DatabaseSeeder::departments();
 
 $factory->define(CompanyDepartment::class, function (Faker $faker) use ($sampleDepartments) {
     return [
